8549d2515602b57d7696dad6ad9bff2a3bdd732a
bbiederman <bbied282@mtroyal.ca>
2016-11-09 14:38:58 -0700

    GUI work. Pulled out toolbar code for simplicity.

--- customer-client/customer-client/MainActivity.cs
+++ customer-client/customer-client/MainActivity.cs
@@ -1,12 +1,15 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace customer_client
 {
     [Activity(Label = "customer_client", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private Button login;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -16,6 +19,21 @@ namespace customer_client
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
             //Test
+
+            findViews();
+            //clickHandler();
+            login.Click += delegate
+            {
+                var gettableactivity = new Android.Content.Intent(this, typeof(GetTableActivity));
+                //extras here
+                StartActivity (gettableactivity);
+            };
+        }
+
+        private void findViews()
+        {
+            login = FindViewById<Button>(Resource.Id.login);
+            //throw new NotImplementedException();
         }
     }
 }
---
+++ customer-client/customer-client/OrderActivity.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Forms;
+
+namespace customer_client
+{
+    [Activity(Label = "Activity1")]
+    public class OrderActivity : Activity
+    {
+        //private List 
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            Xamarin.Forms.ListView menuListView = new Xamarin.Forms.ListView;
+
+            //ItemSource = testList;    
+        }
+    }
+}
