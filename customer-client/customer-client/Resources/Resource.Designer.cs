555754c91175fcf70c15418991628d94d25f74f3
bbiederman <bbied282@mtroyal.ca>
2016-11-11 11:15:32 -0700

    Fixed csproj file to show new resources and activities that Kevin made.

--- customer-client/customer-client/Resources/Resource.Designer.cs
+++ customer-client/customer-client/Resources/Resource.Designer.cs
@@ -47,22 +47,25 @@ namespace customer_client
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int BasicDing = 2130837504;
+			public const int AlsoBasicDing = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int facebook = 2130837505;
+			public const int BasicDing = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int facebook2 = 2130837506;
+			public const int facebook = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int Icon = 2130837507;
+			public const int facebook2 = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int lorem = 2130837508;
+			public const int Icon = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int lorem2 = 2130837509;
+			public const int lorem = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int lorem2 = 2130837510;
 			
 			static Drawable()
 			{
