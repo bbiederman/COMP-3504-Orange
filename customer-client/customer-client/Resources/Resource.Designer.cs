725b39e10a8bf545769e8ace22f183cd37d37e98
bbiederman <bbied282@mtroyal.ca>
2016-11-10 21:05:15 -0700

    Basic ordering functionality is implemented. Menu items can be picked from a listView in OrderActivity (sending them and the user to the OrderTableAct activity), where from the OrderTableAct activity, the menu items can can be long-pressed to see the name and ID or to delete the menuItem from the OrderTableAct activity's listView.

--- customer-client/customer-client/Resources/Resource.Designer.cs
+++ customer-client/customer-client/Resources/Resource.Designer.cs
@@ -74,11 +74,11 @@ namespace customer_client
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000f
-			public const int addItemButton = 2131034127;
+			// aapt resource value: 0x7f050011
+			public const int addItemButton = 2131034129;
 			
-			// aapt resource value: 0x7f050012
-			public const int dingBut = 2131034130;
+			// aapt resource value: 0x7f050014
+			public const int dingBut = 2131034132;
 			
 			// aapt resource value: 0x7f050007
 			public const int editText1 = 2131034119;
@@ -101,20 +101,23 @@ namespace customer_client
 			// aapt resource value: 0x7f050005
 			public const int imageView1 = 2131034117;
 			
-			// aapt resource value: 0x7f05000c
-			public const int itemListView = 2131034124;
-			
 			// aapt resource value: 0x7f05000d
-			public const int label = 2131034125;
+			public const int itemListView = 2131034125;
+			
+			// aapt resource value: 0x7f05000f
+			public const int label = 2131034127;
+			
+			// aapt resource value: 0x7f05000c
+			public const int linearLayout1 = 2131034124;
 			
 			// aapt resource value: 0x7f05000a
 			public const int login = 2131034122;
 			
-			// aapt resource value: 0x7f05000e
-			public const int nameEditText = 2131034126;
-			
 			// aapt resource value: 0x7f050010
-			public const int orderTable = 2131034128;
+			public const int nameEditText = 2131034128;
+			
+			// aapt resource value: 0x7f050012
+			public const int orderTable = 2131034130;
 			
 			// aapt resource value: 0x7f050000
 			public const int relativeLayout1 = 2131034112;
@@ -128,8 +131,11 @@ namespace customer_client
 			// aapt resource value: 0x7f050008
 			public const int textView2 = 2131034120;
 			
-			// aapt resource value: 0x7f050011
-			public const int total = 2131034129;
+			// aapt resource value: 0x7f050013
+			public const int total = 2131034131;
+			
+			// aapt resource value: 0x7f05000e
+			public const int viewOrder = 2131034126;
 			
 			static Id()
 			{
