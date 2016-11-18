af28ef579f7071340c183ffbd1ecc103c51fb297
chizwhiz <chizwhiz@gmail.com>
2016-11-18 13:09:37 -0700

    Filled out some of the new sql calss

--- customer-client/customer-client/sql.cs
+++ customer-client/customer-client/sql.cs
@@ -12,7 +12,39 @@ using Android.Widget;
 
 namespace customer_client
 {
-    class sql
+    public class sql
     {
+        //Variables
+        private List<menuItem> list;
+
+        //Constructor
+        public sql()
+        {
+            createList();
+        }
+
+        //Create List
+        private void createList()
+        {
+            list = new List<menuItem>();
+
+            /*
+             * Loads .csv
+             * Format: name, description, cost, imageID, itemID, status
+             */
+            
+
+        }
+
+        private menuItem buildItem(string name, string description, decimal itemCost, int imageId, int itemId, int status)
+        {
+            menuItem item = new customer_client.menuItem(name, description, itemCost, imageId);
+
+            item.itemStatus = status;
+            //item.setStatus(status);           This doesn't exist right now
+
+            return item;
+        }
+
     }
 }
