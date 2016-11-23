7230580a983278601813ebdbc160a4b9742380a0
bbiederman <bbied282@mtroyal.ca>
2016-11-23 13:51:13 -0700

    Newest?

--- customer-client/customer-client/resSelectAct.cs
+++ customer-client/customer-client/resSelectAct.cs
@@ -12,109 +12,14 @@ using Android.Widget;
 
 namespace customer_client
 {
-    [Activity(Label = "Pick a Restaurant")]
+    [Activity(Label = "resSelectAct")]
     public class resSelectAct : Activity
     {
-        private int resId;
-        private string tableNum;
-        private RadioButton radio_res1;
-        private RadioButton radio_res2;
-        private Button selectBut;
         protected override void OnCreate(Bundle savedInstanceState)
         {
-
-
-
-
-
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.resSelect);
-
-
-            if (Intent.HasExtra("tableNumber"))
-            {
-                //GetTableActivity.Window.SetTitle
-                tableNum = Intent.GetStringExtra("tableNumber");
-
-            }
-
-
-            loadViews();
-
-            //selectBut.Enabled = false;
-
-
-            //radio_res1.Click += RadioRes1Click;
-            //radio_res2.Click += RadioRes2Click;
-
-
-
-
-            selectBut.Click += delegate
-            {
-
-
-
-                //var orderactivity = new Android.Content.Intent(this, typeof(OrderActivity));
-                var OrderAct = new Android.Content.Intent(this, typeof(OrderActivity));
-                //extras here
-
-                OrderAct.PutExtra("resId", resId);
-                StartActivity(OrderAct);
-            };
-
-
-
-
-
-            //butFunction();
-
-
-
-
-
-
+            // Create your application here
         }
-
-
-        public void RadioRes1Click(object sender, EventArgs e)
-        {
-
-            resId = 1;
-            //Button selectBut = FindViewById<Button>(Resource.Id.selectResBut);
-
-            selectBut.Enabled = true;
-
-
-
-
-        }
-
-
-
-
-        public void RadioRes2Click(object sender, EventArgs e)
-        {
-            resId = 2;
-            //Button selectBut = FindViewById<Button>(Resource.Id.selectResBut);
-
-
-
-            selectBut.Enabled = true;
-
-
-
-
-        }
-
-
-        private void loadViews()
-        {
-            radio_res1 = FindViewById<RadioButton>(Resource.Id.radio_R1);
-            radio_res2 = FindViewById<RadioButton>(Resource.Id.radio_R2);
-            selectBut = FindViewById<Button>(Resource.Id.selectResBut);
-        }
-
     }
 }
