502e5c5a7bf28f64fd433b1e59a45377bd13f2fd
chizwhiz <chizwhiz@gmail.com>
2016-11-29 12:22:42 -0700

    Added auth functionality. Cheated w/ hard code, but the hard bits are clearly labeled for later replacement.

--- customer-client/customer-client/MainActivity.cs
+++ customer-client/customer-client/MainActivity.cs
@@ -10,6 +10,8 @@ namespace customer_client
     {
         private Button login;
         private EditText emailAddress;
+        private Button password;
+        private Button guestLogin;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -25,22 +27,77 @@ namespace customer_client
             //clickHandler();
             login.Click += delegate
             {
-                var gettableactivity = new Android.Content.Intent(this, typeof(GetTableActivity));
-                //extras here
-                char[] delimiterChar = { '@' };
-                string username = emailAddress.Text;
-                string[] usernameDelim = username.Split(delimiterChar);
-                gettableactivity.PutExtra("username", usernameDelim[0]);
-                Console.WriteLine(usernameDelim);
-                StartActivity (gettableactivity);
-                
+                //Get entered username & password
+                string usr = emailAddress.Text;
+                string pass = password.Text;
+
+                //Get list of valid user-pass pairs
+                string[,] users = getValidUsers();
+
+                if( credentialsAreValid( usr,pass,users ) )
+                {
+                    submit(usr);
+                }
             };
+
+            //Guest Login
+            guestLogin.Click += delegate
+            {
+                //Skips validation, submits w/ guest username
+                submit("Guest@test.com");
+            };
+        }
+
+        private bool credentialsAreValid(string usr, string pass, string[,] list)
+        {
+            bool result = false;
+
+            for (int x = 0; x < list.GetLength(0); x += 1)
+            {
+                if( list[x,0].Equals(usr) && list[x,1].Equals(pass) )   //If usr-pass pair matches...
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        //Contains hard code. Replace it with real code when possible
+        private string[,] getValidUsers()
+        {
+            //Initialize 2D string array w/ [x,0]=usr & [x,1]=pass
+            string[,] list = new string[,] {};
+
+            //Begin hard code
+            list[0,0] = "usr1";
+            list[0,1] = "pass1";
+            list[1,0] = "usr2";
+            list[1,1] = "pass2";
+            list[2,0] = "usr3";
+            list[2,1] = "pass3";
+            //End hard code
+
+            return list;
+        }
+
+        private void submit(string user)
+        {
+            var gettableactivity = new Android.Content.Intent(this, typeof(GetTableActivity));
+            //extras here
+            char[] delimiterChar = { '@' };
+            string[] usernameDelim = user.Split(delimiterChar);
+            gettableactivity.PutExtra("username", usernameDelim[0]);
+            Console.WriteLine(usernameDelim);
+            StartActivity(gettableactivity);
         }
 
         private void findViews()
         {
             login = FindViewById<Button>(Resource.Id.login);
             emailAddress = FindViewById<EditText>(Resource.Id.emailAddress);
+            password = FindViewById<Button>(Resource.Id.password);
+            guestLogin = FindViewById<Button>(Resource.Id.guestLogin);
             //throw new NotImplementedException();//
         }
     }
--- customer-client/customer-client/Resources/Resource.Designer.cs
+++ customer-client/customer-client/Resources/Resource.Designer.cs
@@ -26,9 +26,6 @@ namespace customer_client
 		
 		public static void UpdateIdValues()
 		{
-			global::ZXing.Mobile.Resource.Id.contentFrame = global::customer_client.Resource.Id.contentFrame;
-			global::ZXing.Mobile.Resource.Layout.zxingscanneractivitylayout = global::customer_client.Resource.Layout.zxingscanneractivitylayout;
-			global::ZXing.Mobile.Resource.Layout.zxingscannerfragmentlayout = global::customer_client.Resource.Layout.zxingscannerfragmentlayout;
 		}
 		
 		public partial class Attribute
@@ -117,23 +114,17 @@ namespace customer_client
 			// aapt resource value: 0x7f050004
 			public const int addButton = 2131034116;
 			
-			// aapt resource value: 0x7f050019
-			public const int addItemButton = 2131034137;
+			// aapt resource value: 0x7f05001a
+			public const int addItemButton = 2131034138;
 			
-			// aapt resource value: 0x7f05001b
-			public const int backToMenu = 2131034139;
-			
-			// aapt resource value: 0x7f050020
-			public const int contentFrame = 2131034144;
+			// aapt resource value: 0x7f05001c
+			public const int backToMenu = 2131034140;
 			
 			// aapt resource value: 0x7f050002
 			public const int description = 2131034114;
 			
-			// aapt resource value: 0x7f05001d
-			public const int dingBut = 2131034141;
-			
-			// aapt resource value: 0x7f050011
-			public const int editText2 = 2131034129;
+			// aapt resource value: 0x7f05001e
+			public const int dingBut = 2131034142;
 			
 			// aapt resource value: 0x7f05000f
 			public const int emailAddress = 2131034127;
@@ -150,32 +141,38 @@ namespace customer_client
 			// aapt resource value: 0x7f050006
 			public const int getTableHeader = 2131034118;
 			
+			// aapt resource value: 0x7f050012
+			public const int guestLogin = 2131034130;
+			
 			// aapt resource value: 0x7f050001
 			public const int image = 2131034113;
 			
-			// aapt resource value: 0x7f050013
-			public const int imageButton1 = 2131034131;
+			// aapt resource value: 0x7f050014
+			public const int imageButton1 = 2131034132;
 			
 			// aapt resource value: 0x7f05000a
 			public const int imageView1 = 2131034122;
 			
-			// aapt resource value: 0x7f050015
-			public const int itemListView = 2131034133;
-			
-			// aapt resource value: 0x7f050017
-			public const int label = 2131034135;
-			
-			// aapt resource value: 0x7f050014
-			public const int linearLayout1 = 2131034132;
-			
-			// aapt resource value: 0x7f050012
-			public const int login = 2131034130;
+			// aapt resource value: 0x7f050016
+			public const int itemListView = 2131034134;
 			
 			// aapt resource value: 0x7f050018
-			public const int nameEditText = 2131034136;
+			public const int label = 2131034136;
 			
-			// aapt resource value: 0x7f05001a
-			public const int orderTable = 2131034138;
+			// aapt resource value: 0x7f050015
+			public const int linearLayout1 = 2131034133;
+			
+			// aapt resource value: 0x7f050013
+			public const int login = 2131034131;
+			
+			// aapt resource value: 0x7f050019
+			public const int nameEditText = 2131034137;
+			
+			// aapt resource value: 0x7f05001b
+			public const int orderTable = 2131034139;
+			
+			// aapt resource value: 0x7f050011
+			public const int password = 2131034129;
 			
 			// aapt resource value: 0x7f050003
 			public const int price = 2131034115;
@@ -183,11 +180,11 @@ namespace customer_client
 			// aapt resource value: 0x7f050005
 			public const int relativeLayout1 = 2131034117;
 			
-			// aapt resource value: 0x7f05001e
-			public const int res1But = 2131034142;
-			
 			// aapt resource value: 0x7f05001f
-			public const int res2But = 2131034143;
+			public const int res1But = 2131034143;
+			
+			// aapt resource value: 0x7f050020
+			public const int res2But = 2131034144;
 			
 			// aapt resource value: 0x7f050009
 			public const int tableSubmit = 2131034121;
@@ -198,11 +195,11 @@ namespace customer_client
 			// aapt resource value: 0x7f050010
 			public const int textView2 = 2131034128;
 			
-			// aapt resource value: 0x7f05001c
-			public const int total = 2131034140;
+			// aapt resource value: 0x7f05001d
+			public const int total = 2131034141;
 			
-			// aapt resource value: 0x7f050016
-			public const int viewOrder = 2131034134;
+			// aapt resource value: 0x7f050017
+			public const int viewOrder = 2131034135;
 			
 			static Id()
 			{
@@ -240,12 +237,6 @@ namespace customer_client
 			
 			// aapt resource value: 0x7f030007
 			public const int resSelect = 2130903047;
-			
-			// aapt resource value: 0x7f030008
-			public const int zxingscanneractivitylayout = 2130903048;
-			
-			// aapt resource value: 0x7f030009
-			public const int zxingscannerfragmentlayout = 2130903049;
 			
 			static Layout()
 			{
