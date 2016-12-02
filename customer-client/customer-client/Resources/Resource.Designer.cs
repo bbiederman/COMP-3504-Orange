d6767bcc17a09e877175d76d1387c9af8e86f19e
Bradley <bbied282@mtroyal.ca>
2016-12-02 13:49:36 -0700

    Added final images and second menu.

--- customer-client/customer-client/Resources/Resource.Designer.cs
+++ customer-client/customer-client/Resources/Resource.Designer.cs
@@ -44,6 +44,22 @@ namespace customer_client
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int theme_devicedefault_background = 2131034112;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
@@ -57,40 +73,73 @@ namespace customer_client
 			public const int burger = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int citrus = 2130837507;
+			public const int chickenTacos = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int facebook = 2130837508;
+			public const int citrus = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int facebook2 = 2130837509;
+			public const int facebook = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int icecream = 2130837510;
+			public const int facebook2 = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int Icon = 2130837511;
+			public const int friedGyoza = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int lorem = 2130837512;
+			public const int glazedGingerBeef = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int lorem2 = 2130837513;
+			public const int icecream = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int OrangeLogo = 2130837514;
+			public const int Icon = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int pizza = 2130837515;
+			public const int lobsterCake = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int QBLogo = 2130837516;
+			public const int lorem = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int soda = 2130837517;
+			public const int lorem2 = 2130837517;
 			
 			// aapt resource value: 0x7f02000e
-			public const int water = 2130837518;
+			public const int OrangeLogo = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int pasta = 2130837519;
+			
+			// aapt resource value: 0x7f020010
+			public const int pizza = 2130837520;
+			
+			// aapt resource value: 0x7f020011
+			public const int primeRib = 2130837521;
+			
+			// aapt resource value: 0x7f020012
+			public const int QBLogo = 2130837522;
+			
+			// aapt resource value: 0x7f020013
+			public const int soda = 2130837523;
+			
+			// aapt resource value: 0x7f020014
+			public const int steakFrites = 2130837524;
+			
+			// aapt resource value: 0x7f020015
+			public const int strawberryMousse = 2130837525;
+			
+			// aapt resource value: 0x7f020016
+			public const int tunaTataki = 2130837526;
+			
+			// aapt resource value: 0x7f020017
+			public const int twoOneBurger = 2130837527;
+			
+			// aapt resource value: 0x7f020018
+			public const int waffleBurger = 2130837528;
+			
+			// aapt resource value: 0x7f020019
+			public const int water = 2130837529;
 			
 			static Drawable()
 			{
@@ -105,107 +154,107 @@ namespace customer_client
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000d
-			public const int Description = 2131034125;
+			// aapt resource value: 0x7f07000d
+			public const int Description = 2131165197;
 			
-			// aapt resource value: 0x7f05000b
-			public const int Thumbnail = 2131034123;
+			// aapt resource value: 0x7f07000b
+			public const int Thumbnail = 2131165195;
 			
-			// aapt resource value: 0x7f05000c
-			public const int Title = 2131034124;
+			// aapt resource value: 0x7f07000c
+			public const int Title = 2131165196;
 			
-			// aapt resource value: 0x7f050004
-			public const int addButton = 2131034116;
+			// aapt resource value: 0x7f070004
+			public const int addButton = 2131165188;
 			
-			// aapt resource value: 0x7f05001a
-			public const int addItemButton = 2131034138;
+			// aapt resource value: 0x7f07001a
+			public const int addItemButton = 2131165210;
 			
-			// aapt resource value: 0x7f05001c
-			public const int backToMenu = 2131034140;
+			// aapt resource value: 0x7f07001c
+			public const int backToMenu = 2131165212;
 			
-			// aapt resource value: 0x7f050021
-			public const int contentFrame = 2131034145;
+			// aapt resource value: 0x7f070021
+			public const int contentFrame = 2131165217;
 			
-			// aapt resource value: 0x7f050002
-			public const int description = 2131034114;
+			// aapt resource value: 0x7f070002
+			public const int description = 2131165186;
 			
-			// aapt resource value: 0x7f05001e
-			public const int dingBut = 2131034142;
+			// aapt resource value: 0x7f07001e
+			public const int dingBut = 2131165214;
 			
-			// aapt resource value: 0x7f05000f
-			public const int emailAddress = 2131034127;
+			// aapt resource value: 0x7f07000f
+			public const int emailAddress = 2131165199;
 			
-			// aapt resource value: 0x7f050008
-			public const int enterTableNumber = 2131034120;
+			// aapt resource value: 0x7f070008
+			public const int enterTableNumber = 2131165192;
 			
-			// aapt resource value: 0x7f050000
-			public const int foodName = 2131034112;
+			// aapt resource value: 0x7f070000
+			public const int foodName = 2131165184;
 			
-			// aapt resource value: 0x7f050007
-			public const int getGRCode = 2131034119;
+			// aapt resource value: 0x7f070007
+			public const int getGRCode = 2131165191;
 			
-			// aapt resource value: 0x7f050006
-			public const int getTableHeader = 2131034118;
+			// aapt resource value: 0x7f070006
+			public const int getTableHeader = 2131165190;
 			
-			// aapt resource value: 0x7f050012
-			public const int guestLogin = 2131034130;
+			// aapt resource value: 0x7f070012
+			public const int guestLogin = 2131165202;
 			
-			// aapt resource value: 0x7f050001
-			public const int image = 2131034113;
+			// aapt resource value: 0x7f070001
+			public const int image = 2131165185;
 			
-			// aapt resource value: 0x7f050014
-			public const int imageButton1 = 2131034132;
+			// aapt resource value: 0x7f070014
+			public const int imageButton1 = 2131165204;
 			
-			// aapt resource value: 0x7f05000a
-			public const int imageView1 = 2131034122;
+			// aapt resource value: 0x7f07000a
+			public const int imageView1 = 2131165194;
 			
-			// aapt resource value: 0x7f050016
-			public const int itemListView = 2131034134;
+			// aapt resource value: 0x7f070016
+			public const int itemListView = 2131165206;
 			
-			// aapt resource value: 0x7f050018
-			public const int label = 2131034136;
+			// aapt resource value: 0x7f070018
+			public const int label = 2131165208;
 			
-			// aapt resource value: 0x7f050015
-			public const int linearLayout1 = 2131034133;
+			// aapt resource value: 0x7f070015
+			public const int linearLayout1 = 2131165205;
 			
-			// aapt resource value: 0x7f050013
-			public const int login = 2131034131;
+			// aapt resource value: 0x7f070013
+			public const int login = 2131165203;
 			
-			// aapt resource value: 0x7f050019
-			public const int nameEditText = 2131034137;
+			// aapt resource value: 0x7f070019
+			public const int nameEditText = 2131165209;
 			
-			// aapt resource value: 0x7f05001b
-			public const int orderTable = 2131034139;
+			// aapt resource value: 0x7f07001b
+			public const int orderTable = 2131165211;
 			
-			// aapt resource value: 0x7f050011
-			public const int password = 2131034129;
+			// aapt resource value: 0x7f070011
+			public const int password = 2131165201;
 			
-			// aapt resource value: 0x7f050003
-			public const int price = 2131034115;
+			// aapt resource value: 0x7f070003
+			public const int price = 2131165187;
 			
-			// aapt resource value: 0x7f050005
-			public const int relativeLayout1 = 2131034117;
+			// aapt resource value: 0x7f070005
+			public const int relativeLayout1 = 2131165189;
 			
-			// aapt resource value: 0x7f05001f
-			public const int res1But = 2131034143;
+			// aapt resource value: 0x7f07001f
+			public const int res1But = 2131165215;
 			
-			// aapt resource value: 0x7f050020
-			public const int res2But = 2131034144;
+			// aapt resource value: 0x7f070020
+			public const int res2But = 2131165216;
 			
-			// aapt resource value: 0x7f050009
-			public const int tableSubmit = 2131034121;
+			// aapt resource value: 0x7f070009
+			public const int tableSubmit = 2131165193;
 			
-			// aapt resource value: 0x7f05000e
-			public const int textView1 = 2131034126;
+			// aapt resource value: 0x7f07000e
+			public const int textView1 = 2131165198;
 			
-			// aapt resource value: 0x7f050010
-			public const int textView2 = 2131034128;
+			// aapt resource value: 0x7f070010
+			public const int textView2 = 2131165200;
 			
-			// aapt resource value: 0x7f05001d
-			public const int total = 2131034141;
+			// aapt resource value: 0x7f07001d
+			public const int total = 2131165213;
 			
-			// aapt resource value: 0x7f050017
-			public const int viewOrder = 2131034135;
+			// aapt resource value: 0x7f070017
+			public const int viewOrder = 2131165207;
 			
 			static Id()
 			{
@@ -275,6 +324,22 @@ namespace customer_client
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int Theme_Custom = 2131099648;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
